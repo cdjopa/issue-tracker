096cd3b905a386f5e61f068a627abab46d991260
josephpanossian <josephpanossian@gmail.com>
2023-12-13 12:47:39 -0800

    [be] update login to use email instead of username

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -7,17 +7,19 @@ export class UsersService {
   private readonly users = [
     {
       userId: 1,
+      email: 'john@gmail.com',
       username: 'john',
       password: 'changeme',
     },
     {
       userId: 2,
+      email: 'maria@gmail.com',
       username: 'maria',
       password: 'guess',
     },
   ];
 
-  async findOne(username: string): Promise<User | undefined> {
-    return this.users.find((user) => user.username === username);
+  async findOne(email: string): Promise<User | undefined> {
+    return this.users.find((user) => user.email === email);
   }
 }
