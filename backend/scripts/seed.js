8d63743aba2a3b456f75d6fcb54f5663674a0d85
josephpanossian <josephpanossian@gmail.com>
2023-12-22 16:30:54 -0800

    remove unique key constraint from project summary

--- backend/scripts/seed.js
+++ backend/scripts/seed.js
@@ -87,12 +87,12 @@ async function createProjectsTable() {
         id UUID DEFAULT uuid_generate_v4() NOT NULL PRIMARY KEY,
         name TEXT UNIQUE NOT NULL,
         project_key TEXT NOT NULL,
-        summary TEXT UNIQUE NOT NULL,
+        summary TEXT,
         start_date TEXT NOT NULL,
-        target_end_date TEXT NOT NULL,
+        target_end_date TEXT,
         actual_end_date TEXT,
         created_on TEXT NOT NULL,
-        created_by UUID NOT NULL
+        created_by UUID NOT NULL REFERENCES users(id)
       );
       `
     )
